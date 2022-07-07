d8e7a01ae6b105ea5e28ab0db5be94ea7580e6e1
mtarikg <mtarikgol@gmail.com>
2022-07-07 21:10:40 +0300

    added generic repository methods

---
+++ InventoryDemo.DataAccess/Repositories/Abstracts/EFGenericRepository.cs
@@ -0,0 +1,66 @@
+﻿using InventoryDemo.DataAccess.Data;
+using InventoryDemo.Entities.Abstracts;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryDemo.DataAccess.Repositories.Abstracts
+{
+    public class EFGenericRepository<T> : IRepository<T> where T : class, IEntity, new()
+    {
+        protected readonly InventoryDbContext dbContext;
+        protected readonly DbSet<T> dbSet;
+
+        public EFGenericRepository(InventoryDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+            dbSet = dbContext.Set<T>();
+        }
+
+        public async Task<int> Add(T entity)
+        {
+            await dbSet.AddAsync(entity);
+            await dbContext.SaveChangesAsync();
+
+            return entity.ID;
+        }
+
+        public async Task Delete(int id)
+        {
+            var entity = await dbSet.FirstOrDefaultAsync(e => e.ID == id);
+            dbSet.Remove(entity);
+            await dbContext.SaveChangesAsync();
+        }
+
+        public async Task<IList<T>> GetAllEntities()
+        {
+            var entities = await dbSet.ToListAsync();
+
+            return entities;
+        }
+
+        public async Task<T> GetEntityByID(int id)
+        {
+            var entity = await dbSet.AsNoTracking().FirstOrDefaultAsync(e => e.ID == id);
+
+            return entity;
+        }
+
+        public async Task<bool> IsExist(int id)
+        {
+            var result = await dbSet.AnyAsync(e => e.ID == id);
+
+            return result;
+        }
+
+        public async Task<int> Update(T entity)
+        {
+            dbSet.Update(entity);
+
+            return await dbContext.SaveChangesAsync();
+        }
+    }
+}
---
+++ InventoryDemo.DataAccess/Repositories/Abstracts/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using InventoryDemo.Entities.Abstracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryDemo.DataAccess.Repositories.Abstracts
+{
+    public interface IRepository<T> where T : class, IEntity, new()
+    {
+        Task<IList<T>> GetAllEntities();
+        Task<T> GetEntityByID(int id);
+        Task<int> Add(T entity);
+        Task<int> Update(T entity);
+        Task Delete(int id);
+        Task<bool> IsExist(int id);
+    }
+}
